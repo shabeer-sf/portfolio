9d82230a3e46d918a3f65b8b16423b40ce8f0126
shabeer-sf <shabeersggs@gmail.com>
2025-03-04 01:22:06 +0530

    Reinitialized Git and added changes after accidental deletion

--- providers/session-provider.jsx
+++
@@ -1,8 +0,0 @@
-// app/providers/session-provider.jsx
-"use client";
-
-import { SessionProvider as NextAuthSessionProvider } from "next-auth/react";
-
-export function SessionProvider({ children }) {
-  return <NextAuthSessionProvider>{children}</NextAuthSessionProvider>;
-}
