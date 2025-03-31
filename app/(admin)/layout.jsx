481965835f5f5a087b522083fe233f04cb7f4841
shabeer-sf <shabeersggs@gmail.com>
2025-04-01 01:17:41 +0530

    admin changes

--- app/(admin)/layout.jsx
+++ app/(admin)/layout.jsx
@@ -1,15 +1,14 @@
 // app/admin/layout.jsx
-import AdminWrapper from '@/components/AdminWrapper';
-import '../globals.css';
-import { dynamic, fetchCache, revalidate } from './config'; // Make sure config.js is in the /admin folder
-import { SearchParamsProvider } from "@/lib/search-params";
+import AdminWrapper from "@/components/AdminWrapper";
+import "../globals.css";
+import { dynamic, fetchCache, revalidate } from "./config"; // Make sure config.js is in the /admin folder
 
 export default function AdminLayout({ children }) {
   return (
     <AdminWrapper>
-      <SearchParamsProvider>
-        {children}
-      </SearchParamsProvider>
+      {/* <SearchParamsProvider> */}
+      {children}
+      {/* </SearchParamsProvider> */}
     </AdminWrapper>
   );
 }
