557e3e4bc1007e479b40a01b678fe338b51d8cf2
shabeer-sf <shabeersggs@gmail.com>
2025-04-01 01:21:09 +0530

    admin changes

--- app/(admin)/layout.jsx
+++ app/(admin)/layout.jsx
@@ -1,14 +1,19 @@
 // app/admin/layout.jsx
-import AdminWrapper from "@/components/AdminWrapper";
-import "../globals.css";
-import { dynamic, fetchCache, revalidate } from "./config"; // Make sure config.js is in the /admin folder
+import { Suspense } from 'react';
+import AdminWrapper from '@/components/AdminWrapper';
+import '../globals.css';
+import { dynamic, fetchCache, revalidate } from './config'; // Make sure config.js is in the /admin folder
+import { SearchParamsProvider } from "@/lib/search-params";
 
 export default function AdminLayout({ children }) {
   return (
     <AdminWrapper>
-      {/* <SearchParamsProvider> */}
-      {children}
-      {/* </SearchParamsProvider> */}
+      {/* Ensure SearchParamsProvider is used correctly with Suspense */}
+      <Suspense fallback={null}>
+        <SearchParamsProvider>
+          {children}
+        </SearchParamsProvider>
+      </Suspense>
     </AdminWrapper>
   );
 }
